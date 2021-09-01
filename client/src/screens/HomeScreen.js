5ce9dc5a030a63ad16a07c11ed023f1130b12491
mirasrobert <mrmirasrobert@gmail.com>
2021-09-01 16:31:36 +0800

    fetch product list from the redux state

--- client/src/screens/HomeScreen.js
+++ client/src/screens/HomeScreen.js
@@ -1,32 +1,38 @@
-import { useState, useEffect } from 'react';
+import { useEffect } from 'react';
 import Product from '../components/products/Product';
 import { Row, Col } from 'react-bootstrap';
-import axios from 'axios';
+import { useDispatch, useSelector } from 'react-redux';
+import { listProducts } from '../actions/productActions';
 
 const HomeScreen = () => {
-  const [products, setProducts] = useState([]);
+  const dispatch = useDispatch(); // hooks to dispatch an action
+
+  // Get from the state
+  const productList = useSelector((state) => state.productList);
+
+  // Desctructure what we need
+  const { loading, error, products } = productList;
 
   useEffect(() => {
-    const fetchProducts = async () => {
-      const { data } = await axios.get('/api/products');
-
-      setProducts(data.products);
-    };
-
-    fetchProducts();
-  }, []);
+    dispatch(listProducts());
+  }, [dispatch]);
 
   return (
     <>
       <h1>Latest Products</h1>
-      <Row>
-        {products.length > 0 &&
-          products.map((product) => (
+      {loading ? (
+        <h2>Loading...</h2>
+      ) : error ? (
+        <h2>{error}</h2>
+      ) : (
+        <Row>
+          {products.map((product) => (
             <Col key={product.id} sm='12' md='6' lg='4' xl='3'>
               <Product product={product} />
             </Col>
           ))}
-      </Row>
+        </Row>
+      )}
     </>
   );
 };
